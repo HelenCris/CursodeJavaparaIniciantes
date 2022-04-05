8ea29e2a6cc2184fc39895dbe248438d20820af8
Helen Cris <helencris0110@hotmail.com>
2022-04-05 12:22:33 -0300

    Math Class

--- src/MathClass.java
+++ src/MathClass.java
@@ -1,5 +1,11 @@
 public class MathClass {
     public static void main(String[] main) {
-
+        int result = Math.round(1.1F); //return a long
+        int result2 = (int)Math.ceil(1.1F);
+        int result3 = (int) Math.floor(1.1F);
+        int result4 = Math.max(1, 2);
+        int result5 = Math.min(1, 2);
+        double result6 = Math.random() * 100;
+        int result7 = (int) Math.round(Math.random() * 100);
     }
 }
