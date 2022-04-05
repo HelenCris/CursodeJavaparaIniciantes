3575eb51489a0d385332b01953d9a1d818c5ebab
Helen Cris <helencris0110@hotmail.com>
2022-04-05 10:27:03 -0300

    arrays and multidimensional arrays

---
+++ src/Arrays.java
@@ -0,0 +1,11 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int[][] numbers = {{2, 3},{5, 1, 4}};
+        System.out.println(Arrays.deeptoString(numbers));
+        // System.out.println(Arrays.sort(numbers));
+    }
+    
+}
+
+
+
