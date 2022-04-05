468cce139b3c10620b6c070b84307f993cca0deb
Helen Cris <helencris0110@hotmail.com>
2022-04-05 11:52:51 -0300

    ArithmeticExpressions

---
+++ src/ArithmeticExpressions.java
@@ -0,0 +1,14 @@
+public class ArithmeticExpressions {
+    public static void main(String[] arrays) {
+        // + - * / 
+        double result = (double)10 / (double)3;
+        int x = 1;
+        x++;
+        System.out.println(result);
+        System.out.println(x);
+        
+    }
+    
+    
+}
+
