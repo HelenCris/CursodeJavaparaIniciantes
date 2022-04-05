66f182c21d3c77977c97ffd3e00c290e7c1fc027
Helen Cris <helencris0110@hotmail.com>
2022-04-05 13:36:37 -0300

    Logical Operators

---
+++ src/LogicalOperators.java
@@ -0,0 +1,9 @@
+public class LogicalOperators {
+    public static void main(String[] args) {
+        // && || 
+       boolean hasHighIncome = false;
+       boolean hasGoodCredit = true;
+       boolean hasCriminalRecord = false;
+       boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord ; 
+    }
+}
