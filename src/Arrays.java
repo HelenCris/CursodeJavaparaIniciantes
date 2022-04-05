0bb94eef92d6560af17cbb16e2da559e45705328
Helen Cris <helencris0110@hotmail.com>
2022-04-05 10:31:00 -0300

    Const

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,11 +1,14 @@
 public class Arrays {
     public static void main(String[] args) {
         int[][] numbers = {{2, 3},{5, 1, 4}};
-        System.out.println(Arrays.deeptoString(numbers));
+        System.out.println(numbers.length);
         // System.out.println(Arrays.sort(numbers));
     }
     
 }
+
+
+
 
 
 
